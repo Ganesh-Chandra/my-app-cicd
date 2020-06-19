fce1cd507ba216a13bc8ea3c76910a1b638f8cce
Ganesh-Chandra <ganeshchandrasaladi@gmail.com>
2020-06-19 18:36:45 +0530

    small commit to check the working of ci/cd [kind of feels awesomegit status as learning new thinggit status]

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload."[already done a few!!]"
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React! You already knew some htings but You have to reviw the again!!
         </a>
       </header>
     </div>
